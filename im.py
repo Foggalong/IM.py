afba23fbec1706a91f215d2ec2f19eec82576e50
Joshua Fogg <joshua.h.fogg@gmail.com>
2019-02-04 21:55:44 +0000

    Added stats code Code isn't functional, but copied in from an earlier Manchester only version. To be updated as required for the generic version.

--- im.py
+++ im.py
@@ -13,10 +13,16 @@ visitor to begin a chat or not.
 from csv import reader                  # data stored in CSV files
 from datetime import date               # date stamped data files
 from filecmp import cmp                 # comparing status gifs
+from numpy import arange                # barcode plot x-axis
 from os import makedirs, path           # verifying log directory
 from sys import stdout                  # reuse output lines
 from time import strftime, sleep        # time formats, timed execution
 from urllib.request import urlretrieve  # download status gifs
+
+# plotting on headless server
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
 
 
 # SERVICES DATA
@@ -93,6 +99,60 @@ while True:
             log = open(logdir + logfile + ".csv", "a+")
             statusCheck(log, service)
             log.close()
+
+    # TODO: this code isn't functional, copied from Manchester-specific version
+    elif currentTime == "09:00":
+        print("Beginning data analysis")
+
+        # This files is the log from the most recent shift
+        with open("logs/" + logfile, newline="") as log:
+            log = list(reader(log, delimiter=","))
+            logTimes = [entry[0] for entry in log]
+            logStati = [1 if entry[1] == "online" else 0 for entry in log]
+
+        # This file contains a cummulative record from all logs
+        with open("stats/data.csv", newline="") as data:
+            stats = list(reader(data, delimiter=","))
+            statTimes = [entry[0] for entry in stats]
+            statTally = [int(entry[1]) for entry in stats]
+            statTotal = [int(entry[2]) for entry in stats]
+
+        # Add log status' to the cummulative record
+        for x in range(0, len(logTimes)):
+            ind = statTimes.index(logTimes[x])
+            if logStati[x] == 1:
+                statTally[ind] += 1
+            statTotal[ind] += 1
+
+        # Calculate probabilities
+        statCount, statProbs = [x+1 for x in range(0, len(statTotal))], []
+        for ind in range(0, len(statTotal)):
+            statProbs.append(statTally[ind]/statTotal[ind])
+
+        # Writes updated data back to the cummulative record
+        with open("stats/data.csv", "w+") as data:
+            for x in range(0, len(statTimes)):
+                dataLine = [statTimes[x], str(statTally[x]), str(statTotal[x])]
+                data.write(",".join(dataLine)+"\n")
+
+        # Plots uptime for given day
+        fig1 = plt.figure()
+        y = logStati
+        x = arange(len(logStati))
+        plt.bar(x, y, width=1.0)
+        plt.title(logfile + " between " + logTimes[0] + " & " + logTimes[-1])
+        plt.axis('off')
+        plt.savefig("stats/" + logfile + ".png")
+
+        # Plots probability scatter graph
+        fig2 = plt.figure()
+        plt.plot(statCount, statProbs)
+        plt.title("Graph of Chance of IM Availability")
+        plt.xlabel("Time")
+        plt.ylabel("Probability")
+        plt.savefig("stats/probs.png")
+
+        print("Data analysis complete!")
 
     else:
         print("Outside opening hours")
